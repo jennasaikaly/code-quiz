5b571c2397ab8ab1195e94aeb2e147dead52fc3e
jennasaikaly <jen.saikaly@gmail.com>
2022-03-20 21:41:56 -0400

    added css

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,49 +17,57 @@ let quizWrapperEl = document.querySelector('#quiz-wrapper')
 //QUESTION ARRAY
 let allQuestions = [
     {
-    question: "question 1",
-    choices: [  "option 1",
-                "option 2",
-                "option 3",
-                "option 4"],
-    correctAnswer: 1 //index of the correct answer
+    question: "What is not a primitive Data Type in JavaScript?",
+    choices: [  "Boolean",
+                "String",
+                "Float",
+                "Char"],
+    correctAnswer: 1 //index of the correct answer 'string'
     },
     {
-    question: "question 2",
-    choices: [  "option 1",
-                "option 2",
-                "option 3",
-                "option 4"],
-    correctAnswer: 1 //index of the correct answer
+    question: "Which element does JavaScript get entered into?",
+    choices: [  "<scripting>",
+                "<body>",
+                "<javascript>",
+                "<script>"],
+    correctAnswer: 2 //index of the correct answer 'script'
     },
     {
-    question: "question 3",
-    choices: [  "option 1",
-                "option 2",
-                "option 3",
-                "option 4"],
-    correctAnswer: 1 //index of the correct answer
+    question: "What is a useful tool used during development and debugging for printing content to the debugger?",
+    choices: [  "JavaScript",
+                "terminal/bash",
+                "alerts",
+                "console.log"],
+    correctAnswer: 3 //index of the correct answer 'console.log'
     },
     {
-    question: "question 4",
-   choices: [  "option 1",
-                "option 2",
-                "option 3",
-                "option 4"],
-    correctAnswer: 1 //index of the correct answer
+    question: "String values must be enclosed within what when being assigned to variables?",
+   choices: [  "parenthesis",
+                "curly brackets",
+                "quotes",
+                "commas"],
+    correctAnswer: 2 //index of the correct answer "quotes"
     },
     {
-    question: "question 5",
-    choices: [  "option 1",
-                "option 2",
-                "option 3",
-                "option 4"],
-    correctAnswer: 1 //index of the correct answer
+    question: "When writing an 'if/else' statement, what is the condition enclosed in?",
+    choices: [  "curly brackets",
+                "square brackets",
+                "quotes",
+                "parentheses"],
+    correctAnswer: 3 //index of the correct answer 'parentheses'
     },
 ]
-
-
+//function that starts once 'Start' Button is clicked
+let startQuizHandler = function(event){
+  //debugger;
+ event.preventDefault(); //prevents default browser settings from reloading the browser
+//console.log(event.target)
+introEl.remove();
+createQuestion(questionIdCounter);
+}
 function startTimer(){
+
+  
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
     
@@ -69,15 +77,14 @@ function startTimer(){
   //setInterval(startTimer, 1000);
 }
 
-function createQuestion(id){
-        
-   
-    //create question header
+function createQuestion(){
+  
+     //create question header
     let questionWrapperEl = document.createElement("div");
    
     questionWrapperEl.className = "question-wrapper";
-    //add 
-    questionWrapperEl.setAttribute("data-question-id", questionIdCounter);
+    //add question id as a custom attribute
+    questionWrapperEl.setAttribute("data-question-id",questionIdCounter);
 
     let questionListEl = document.createElement("ol");
     questionListEl.className = "quiz-question";
@@ -124,8 +131,8 @@ choiceLi2.appendChild(buttonLi2);
 choiceLi3.appendChild(buttonLi3);
 choiceLi4.appendChild(buttonLi4);
 
-
-let i = 0;
+ 
+let i = questionIdCounter;
 if (i < allQuestions.length) {
     console.log(allQuestions[i].question);
     questionHeaderEl.textContent = allQuestions[i].question;
@@ -134,17 +141,17 @@ if (i < allQuestions.length) {
     buttonLi3.textContent = allQuestions[i].choices[2];
     buttonLi4.textContent = allQuestions[i].choices[3];
     }
+    
+    buttonLi1.addEventListener("click", createQuestion);
+    buttonLi2.addEventListener("click", createQuestion);
+    buttonLi3.addEventListener("click", createQuestion);
+    buttonLi4.addEventListener("click", createQuestion);
+
+    questionIdCounter++;
 }   
 
 
-//function that starts once 'Start' Button is clicked
-let startQuizHandler = function(event){
-  //debugger;
- event.preventDefault(); //prevents default browser settings from reloading the browser
-//console.log(event.target)
- introEl.remove();
-createQuestion();
-}
+
 
 // creates event listener for the start button
 startButtonEl.addEventListener("click", startTimer);
